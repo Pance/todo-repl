eab4225a426ef9ee57244fddc60d83c998148aef
Pance <pbarnett888@gmail.com>
2013-06-24 18:59:34 -0500

    Added functions to create tasks

--- src/todo_repl/core.clj
+++ src/todo_repl/core.clj
@@ -1,6 +1,19 @@
 (ns todo-repl.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn new-task [name due-date context]
+  {:name name
+   :due due-date
+   :context context
+   :created (str (java.util.Date.))})
+
+;(new-task "Do the dishes" "today")
+
+(defn new-task-better [{:keys [name due context]
+                   :or {due "never"
+                        context ""}}]
+  (new-task name due context))
+
+
+;; I want a function that evals ":name do the dishes :due today"
+;; to {:name "do the dishes" :due "today"}
+
