eecec002253d1210c56ff99258f954e9add5e846
Pance <pbarnett888@gmail.com>
2013-07-15 20:44:09 -0500

    Adding url attrib to tasks on creation. Fixes #1

--- src/todo-repl/core.clj
+++ src/todo-repl/core.clj
@@ -1,19 +1,25 @@
 (ns todo-repl.core)
 
-(defn new-task [name due-date context]
+(defn new-task [name due-by due-after context url]
   {:name name
-   :due due-date
+	 :due-after due-after
+   :due-by due-by
    :context context
-   :created (str (java.util.Date.))})
+	 :url []
+   :created (str (java.util.Date.))
+	 })
 
 ;(new-task "Do the dishes" "today")
 
-(defn new-task-better [{:keys [name due context]
-                   :or {due "never"
-                        context ""}}]
-  (new-task name due context))
+(defn new-task-better [{:keys [name due-after due-by context url]
+                   :or {due-by "never"
+												due-after "never"
+                        context nil
+												url [{"" "http://"}]}}]
+  (new-task name due-after due-by context url))
 
+(defn add-new-task-to [x y]
+	(cons (new-task-better x) y))
 
 ;; I want a function that evals ":name do the dishes :due today"
 ;; to {:name "do the dishes" :due "today"}
-
