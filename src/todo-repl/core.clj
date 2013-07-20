af2fab5d1b0b727d8fc4c7a294d83b7afa38ebb2
Pance <pbarnett888@gmail.com>
2013-07-20 10:44:46 -0500

    Adding filter function and a function to mark tasks as complete

--- src/todo-repl/core.clj
+++ src/todo-repl/core.clj
@@ -6,20 +6,22 @@
    :due-by due-by
    :context context
 	 :url []
+	 :status :incomplete
    :created (str (java.util.Date.))
 	 })
 
-;(new-task "Do the dishes" "today")
-
 (defn new-task-better [{:keys [name due-after due-by context url]
                    :or {due-by "never"
-												due-after "never"
+												due-after nil
                         context nil
-												url [{"" "http://"}]}}]
+												url [{:label "" :url ""}] }}]
   (new-task name due-after due-by context url))
 
-(defn add-new-task-to [x y]
-	(cons (new-task-better x) y))
+(defn add-new-task-to [new-task tasks]
+	(cons (new-task-better new-task) tasks))
 
-;; I want a function that evals ":name do the dishes :due today"
-;; to {:name "do the dishes" :due "today"}
+(defn filter-tasks [filter-attrib val tasks]
+	(filter #(= val (filter-attrib %)) tasks))
+
+(defn complete-task [& xs]
+	(map #(assoc % :status :complete)) xs)
