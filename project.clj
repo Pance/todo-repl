9fc07e798c55cb2cd0542a95c0d5f6e10796e1c7
Pance <pbarnett888@gmail.com>
2013-08-03 21:43:21 -0500

    Set todo-repl.core and todo-repl.core-test to be loaded automatically in lein repl

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.ocpsoft.prettytime/prettytime-nlp "3.0.2.Final"]])
+                 [org.ocpsoft.prettytime/prettytime-nlp "3.0.2.Final"]]
+  :repl-options {:init (do
+                          (load "/todo-repl/core")
+                          (use 'todo-repl.core)
+                          (load-file "test/todo-repl/core_test.clj")
+                          (use 'todo-repl.core-test))})
