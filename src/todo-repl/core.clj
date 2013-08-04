a7f1413e41377622f4bb6c3ad6d82a5e3964ad03
Pance <pbarnett888@gmail.com>
2013-08-03 21:45:23 -0500

    Reformat tabs to spaces. Move date parsing function to other file

--- src/todo-repl/core.clj
+++ src/todo-repl/core.clj
@@ -1,34 +1,30 @@
 (ns todo-repl.core
-	(:import (org.ocpsoft.prettytime.nlp PrettyTimeParser)))
+    (:load "/todo-repl/date-parsing")
+    (:require [todo-repl.date-parsing :as dp])
+    #_(:use todo-repl.date-parsing))
 
 (defn new-task [name due-by due-after context url]
   {:name name
-	 :due-after due-after
+   :due-after due-after
    :due-by due-by
    :context context
-	 :url []
-	 :status :incomplete
+   :url url
+   :status :incomplete
    :created (str (java.util.Date.))
-	 })
+   })
 
 (defn new-task-better [{:keys [name due-after due-by context url]
                    :or {due-by "never"
-												due-after nil
+                        due-after nil
                         context nil
-												url [{:label "" :url ""}] }}]
-  (new-task name (nl-to-date due-by) (nl-to-date due-after) context url))
+                        url [{:label "" :url ""}] }}]
+  (new-task name (dp/nl-to-date due-by) (dp/nl-to-date due-after) context url))
 
 (defn add-new-task-to [new-task tasks]
-	(cons (new-task-better new-task) tasks))
+  (cons (new-task-better new-task) tasks))
 
 (defn filter-tasks [filter-attrib val tasks]
-	(filter #(= val (filter-attrib %)) tasks))
+  (filter #(= val (filter-attrib %)) tasks))
 
 (defn complete-task [& xs]
-	(map #(assoc % :status :complete)) xs)
-
-(defn nl-to-date [nl]
-	(..
-		(new PrettyTimeParser)
-		(parse nl)
-		(get 0)))
+  (map #(assoc % :status :complete)) xs)
