520c69d9867ac255a2c207902d0d833c9fd109ed
Pance <pbarnett888@gmail.com>
2013-08-03 21:44:11 -0500

    Reformat tabs to spaces

--- test/todo-repl/core_test.clj
+++ test/todo-repl/core_test.clj
@@ -3,22 +3,22 @@
         todo-repl.core))
 
 (def test-tasklist
-	(map new-task-better [{:name "Do the dishes"
-												 :due-by "1-1-2000"
-												 :context "cleaning"}
-												{:name "Wipe the kitchen counters"
-												 :due-by "1-1-2000"
-												 :context "cleaning"}
-												{:name "Call back that dude"
-												 :due-after "1-3-2000"
-												 :context "contact"}
-												{:name "Go to the grocery store"
-												 :due-after "1-1-2000"
-												 :context "errands"}
-												{:name "Check out this mix"
-												 :context "music"
-												 :url [{:label "link"
-																:url "https://soundcloud.com/"}]}]))
+  (map new-task-better [{:name "Do the dishes"
+                         :due-by "1-1-2000"
+                         :context "cleaning"}
+                        {:name "Wipe the kitchen counters"
+                         :due-by "1-1-2000"
+                         :context "cleaning"}
+                        {:name "Call back that dude"
+                         :due-after "1-3-2000"
+                         :context "contact"}
+                        {:name "Go to the grocery store"
+                         :due-after "1-1-2000"
+                         :context "errands"}
+                        {:name "Check out this mix"
+                         :context "music"
+                         :url [{:label "link"
+                                :url "https://soundcloud.com/"}]}]))
 
 
 (deftest a-test
